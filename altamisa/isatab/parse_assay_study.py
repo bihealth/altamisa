bihealth/altamisa

commit 2892ffb89b0f913a68d2ec909743194804fc1039

Add exception for invalid encodings (#88)
Co-authored-by: Mathias Kuhring <mathias.kuhring@bih-charite.de>
Co-authored-by: Thomas Sell <thomas.sell@bih-charite.de>

altamisa/isatab/parse_assay_study.py
@@ -848,6 +848,9 @@ class StudyRowReader:
                 self.unique_rows.add("\t".join(self._line))
         except StopIteration:
             self._line = None
+        except UnicodeDecodeError as e:  # pragma: no cover
+            msg = f"Invalid encoding of study file '{self._filename}' (use Unicode/UTF-8)."
+            raise ParseIsatabException(msg) from e
         return prev_line
 
     def read(self):
@@ -970,6 +973,9 @@ class AssayRowReader:
                 self.unique_rows.add("\t".join(self._line))
         except StopIteration:
             self._line = None
+        except UnicodeDecodeError as e:  # pragma: no cover
+            msg = f"Invalid encoding of assay file '{self._filename}' (use Unicode/UTF-8)."
+            raise ParseIsatabException(msg) from e
         return prev_line
 
     def read(self):

altamisa/isatab/parse_investigation.py
@@ -128,6 +128,9 @@ class InvestigationReader:
                 self._line = list_strip(next(self._reader))
         except StopIteration:
             self._line = None
+        except UnicodeDecodeError as e:  # pragma: no cover
+            msg = f"Invalid encoding of investigation file '{self._filename}' (use Unicode/UTF-8)."
+            raise ParseIsatabException(msg) from e
         return prev_line
 
     def _next_line_startswith_comment(self):

commit 84f4056161ad781ba555426ca84ebbd898dcfd74

fix: Inconsistent attribute naming introduced in #88

altamisa/isatab/parse_assay_study.py
@@ -817,7 +817,7 @@ class StudyRowReader:
     def __init__(self, study_id: str, input_file: TextIO, filename: Optional[str]):
         self.study_id = study_id
         self.input_file = input_file
-        self.filename = filename or getattr(input_file, "name", "<no file>")
+        self._filename = filename or getattr(input_file, "name", "<no file>")
         self.unique_rows = set()
         self.duplicate_rows = []
         self._reader = csv.reader(input_file, delimiter="\t", quotechar='"')
@@ -859,7 +859,7 @@ class StudyRowReader:
 
         :returns: Nodes per row of the study file
         """
-        builder = _StudyRowBuilder(self.header, self.filename, self.study_id)
+        builder = _StudyRowBuilder(self.header, self._filename, self.study_id)
         while True:
             line = self._read_next_line()
             if line:
@@ -942,7 +942,7 @@ class AssayRowReader:
         self.study_id = study_id
         self.assay_id = assay_id
         self.input_file = input_file
-        self.filename = filename or getattr(input_file, "name", "<no file>")
+        self._filename = filename or getattr(input_file, "name", "<no file>")
         self.unique_rows = set()
         self.duplicate_rows = []
         self._reader = csv.reader(input_file, delimiter="\t", quotechar='"')
@@ -984,7 +984,7 @@ class AssayRowReader:
 
         :return: Nodes per row of the assay file
         """
-        builder = _AssayRowBuilder(self.header, self.filename, self.study_id, self.assay_id)
+        builder = _AssayRowBuilder(self.header, self._filename, self.study_id, self.assay_id)
         while True:
             line = self._read_next_line()
             if line: