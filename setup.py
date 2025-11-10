d9db237a1e2ade2f67eb7d9f7b4f943da6854124
Thomas Sell <thomas.sell@bih-charite.de>
2025-11-10 11:35:37 +0100

    chore: update tested Python versions

--- setup.py
+++ setup.py
@@ -49,10 +49,11 @@ setup(
         "Natural Language :: English",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     entry_points={
         "console_scripts": (
